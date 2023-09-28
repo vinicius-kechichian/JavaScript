2d41044797d3dbc1af2b503dcaa0ed17df36b31a
Vinicius Kechichian <134213761+vinicius-kechichian@users.noreply.github.com>
2023-09-28 06:48:30 -0300

    Mod D - aula 11

--- modulo D/aula11/exe02.js
+++ modulo D/aula11/exe02.js
@@ -1,4 +1,6 @@
 var vel = 60.5
-console.log(`a velocidade é ${vel}`)
-
+console.log(`a velocidade é ${vel}Km/h`)
+if (vel > 60) { // Condição simples
+    console.log(`Você ultrapassou a velocidade permitida. MULTADO!`) 
+}
 console.log(`Diriga sempre usando cinto de segurança`)
