6682a607a927fce14de3f2d611096f38f6195f72
Vinicius Kechichian <134213761+vinicius-kechichian@users.noreply.github.com>
2023-10-06 01:52:05 -0300

    Mod E - exe 01

---
+++ modulo E/aula-13/exe01.js
@@ -0,0 +1,13 @@
+var c = 1 
+while (c <= 6) { //while sigmifica "Enquanto"
+    console.log('Tudo bem?')
+    c++ // c = c + 1 
+}
+
+/*-------------------*/
+
+var contador = 1 
+do { //faça, exexuta
+    console.log(`Passo ${contador}`)              
+    (contador <= 6)
+} while (contador <= 6)
