2dc67aafc9e81af31f070b93393f323cfee989f0
Vinicius Kechichian <134213761+vinicius-kechichian@users.noreply.github.com>
2023-10-07 20:19:21 -0300

    mod E - exer 01 pt 01

---
+++ modulo E/aula-14/exe01.js
@@ -0,0 +1,29 @@
+var c = 1 
+while (c <= 5) { // C é menor ou igual a 5
+    console.log(c)
+    c++ // incrementa mais 1 
+}
+
+/* Outro metodo de fazer */
+console.log('outro metodo')
+
+var b = 1
+do {
+    console.log(b)
+    b++
+} while (b <= 5)
+
+
+/* Exemplo dos exercicios 
+   acima usando metodo for */
+console.log('Exemplo dos exercicio acima, usando for')
+
+for (var a = 1; a <= 5; a++) {
+    console.log(a)
+}
+
+console.log('Vai começar..')
+    for (var d = 1; d <= 5; d++){
+    console.log(d)
+}
+console.log('Fim')
